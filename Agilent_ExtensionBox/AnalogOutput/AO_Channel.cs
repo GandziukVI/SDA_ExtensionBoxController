f13b53320f9a25ecd3401d4be276d73ddad44193
zigorrom <zigorrom@gmail.com>
2015-08-12 10:18:38 +0200

    Channels

---
+++ Agilent_ExtensionBox/AnalogOutput/AO_Channel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Agilent_ExtensionBox.AnalogOutput
+{
+    class AO_Channel
+    {
+    }
+}
---
+++ Agilent_ExtensionBox/DigitalIO/DigitalChannel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Agilent_ExtensionBox.DigitalIO
+{
+    class DigitalChannel
+    {
+    }
+}
