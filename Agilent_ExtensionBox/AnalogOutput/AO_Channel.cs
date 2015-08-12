9456b21d9fb705fc7b707f687d3168bbd6c8ab8d
GandziukVI <gandziuk2009@gmail.com>
2015-08-12 18:47:40 +0200

    AO channel implemented

--- Agilent_ExtensionBox/AnalogOutput/AO_Channel.cs
+++ Agilent_ExtensionBox/AnalogOutput/AO_Channel.cs
@@ -10,6 +10,12 @@ namespace Agilent_ExtensionBox.IO
         private string _channelName;
         private AgilentU254xClass _driver;
 
+        private DigitalChannels _DigitalChannels;
+
+        private DigitalBit _Selector_A0;
+        private DigitalBit _Selector_A1;
+        private DigitalBit _Selector_A2;
+        private DigitalBit _Enable;
 
         public AO_Channel(AnalogOutChannelsEnum Channel, AgilentU254xClass Driver)
         {
@@ -21,7 +27,31 @@ namespace Agilent_ExtensionBox.IO
                 default:
                     throw new ArgumentException();
             }
+            
             _driver = Driver;
+            _DigitalChannels = new DigitalChannels(_driver);
+
+            switch (Channel)
+            {
+                case AnalogOutChannelsEnum.AOut1:
+                    {
+                        _Selector_A0 = new DigitalBit(_DigitalChannels[DigitalChannelsEnum.DIOA], 4);
+                        _Selector_A1 = new DigitalBit(_DigitalChannels[DigitalChannelsEnum.DIOA], 5);
+                        _Selector_A2 = new DigitalBit(_DigitalChannels[DigitalChannelsEnum.DIOA], 6);
+
+                        _Enable = new DigitalBit(_DigitalChannels[DigitalChannelsEnum.DIOA], 7);
+                    } break;
+                case AnalogOutChannelsEnum.AOut2:
+                    {
+                        _Selector_A0 = new DigitalBit(_DigitalChannels[DigitalChannelsEnum.DIOA], 0);
+                        _Selector_A1 = new DigitalBit(_DigitalChannels[DigitalChannelsEnum.DIOA], 1);
+                        _Selector_A2 = new DigitalBit(_DigitalChannels[DigitalChannelsEnum.DIOA], 2);
+
+                        _Enable = new DigitalBit(_DigitalChannels[DigitalChannelsEnum.DIOA], 3);
+                    } break;
+                default:
+                    break;
+            }
 
             _driver.AnalogOut.set_Polarity(_channelName, AgilentU254xAnalogPolarityEnum.AgilentU254xAnalogPolarityBipolar);
         }
@@ -39,6 +69,38 @@ namespace Agilent_ExtensionBox.IO
         private void _Set_Enabled(bool Enabled)
         {
             _driver.AnalogOut.set_Enabled(_channelName, Enabled);
+            
+            if (Enabled)
+                _Enable.Set();
+            else
+                _Enable.Reset();
+        }
+
+        private void _Set_ActiveChennel(BOX_AnalogOutChannelsEnum Channel)
+        {
+            switch (Channel)
+            {
+                case BOX_AnalogOutChannelsEnum.BOX_AOut_01: { _Selector_A0.Reset(); _Selector_A1.Reset(); _Selector_A2.Reset(); } break;
+                case BOX_AnalogOutChannelsEnum.BOX_AOut_02: { _Selector_A0.Set(); _Selector_A1.Reset(); _Selector_A2.Reset(); } break;
+                case BOX_AnalogOutChannelsEnum.BOX_AOut_03: { _Selector_A0.Reset(); _Selector_A1.Set(); _Selector_A2.Reset(); } break;
+                case BOX_AnalogOutChannelsEnum.BOX_AOut_04: { _Selector_A0.Set(); _Selector_A1.Set(); _Selector_A2.Reset(); } break;
+                case BOX_AnalogOutChannelsEnum.BOX_AOut_05: { _Selector_A0.Reset(); _Selector_A1.Reset(); _Selector_A2.Set(); } break;
+                case BOX_AnalogOutChannelsEnum.BOX_AOut_06: { _Selector_A0.Set(); _Selector_A1.Reset(); _Selector_A2.Set(); } break;
+                case BOX_AnalogOutChannelsEnum.BOX_AOut_07: { _Selector_A0.Reset(); _Selector_A1.Set(); _Selector_A2.Set(); } break;
+                case BOX_AnalogOutChannelsEnum.BOX_AOut_08: { _Selector_A0.Set(); _Selector_A1.Set(); _Selector_A2.Set(); } break;
+                case BOX_AnalogOutChannelsEnum.BOX_AOut_09: { _Selector_A0.Reset(); _Selector_A1.Reset(); _Selector_A2.Reset(); } break;
+                case BOX_AnalogOutChannelsEnum.BOX_AOut_10: { _Selector_A0.Set(); _Selector_A1.Reset(); _Selector_A2.Reset(); } break;
+                case BOX_AnalogOutChannelsEnum.BOX_AOut_11: { _Selector_A0.Reset(); _Selector_A1.Set(); _Selector_A2.Reset(); } break;
+                case BOX_AnalogOutChannelsEnum.BOX_AOut_12: { _Selector_A0.Set(); _Selector_A1.Set(); _Selector_A2.Reset(); } break;
+                case BOX_AnalogOutChannelsEnum.BOX_AOut_13: { _Selector_A0.Reset(); _Selector_A1.Set(); _Selector_A2.Set(); } break;
+                case BOX_AnalogOutChannelsEnum.BOX_AOut_14: { _Selector_A0.Set(); _Selector_A1.Set(); _Selector_A2.Set(); } break;
+                case BOX_AnalogOutChannelsEnum.BOX_AOut_15: { _Selector_A0.Set(); _Selector_A1.Reset(); _Selector_A2.Set(); } break;
+                case BOX_AnalogOutChannelsEnum.BOX_AOut_16: { _Selector_A0.Reset(); _Selector_A1.Reset(); _Selector_A2.Set(); } break;
+
+
+                default:
+                    throw new ArgumentException();
+            }
         }
 
         private double _Voltage = 0.0;
@@ -60,6 +122,17 @@ namespace Agilent_ExtensionBox.IO
             {
                 _Set_Enabled(value);
                 _Enabled = value;
+            }
+        }
+
+        private BOX_AnalogOutChannelsEnum _OutputNumber = BOX_AnalogOutChannelsEnum.BOX_AOut_01;
+        public BOX_AnalogOutChannelsEnum OutputNumber
+        {
+            get { return _OutputNumber; }
+            set
+            {
+                _Set_ActiveChennel(value);
+                _OutputNumber = value;
             }
         }
     }
