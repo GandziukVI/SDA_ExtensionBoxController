584ef0a478e14643233670611cc564de0cbb5912
GandziukVI <gandziuk2009@gmail.com>
2015-08-12 18:57:21 +0200

    AO_Channels implemented

--- Agilent_ExtensionBox/AnalogOutput/AO_Channels.cs
+++ Agilent_ExtensionBox/AnalogOutput/AO_Channels.cs
@@ -1,10 +1,28 @@
-﻿using System;
+﻿using Agilent.AgilentU254x.Interop;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Agilent_ExtensionBox.IO
 {
-    class AO_Channels
+    public class AO_Channels
     {
+        private AO_Channel[] _channels;
+        public AO_Channels(AgilentU254xClass Driver)
+        {
+            _channels = new AO_Channel[2]
+            {
+                new AO_Channel(AnalogOutChannelsEnum.AOut1, Driver),
+                new AO_Channel(AnalogOutChannelsEnum.AOut2, Driver)
+            };
+        }
+
+        public AO_Channel this[AnalogOutChannelsEnum index]
+        {
+            get
+            {
+                return _channels[(int)index];
+            }
+        }
     }
 }
