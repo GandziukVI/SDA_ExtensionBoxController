dc176059afb61fb47103d954e1f1c1cdab52951e
zigorrom <https://github.com/zigorrom/DigitalAnalyzer.git>
2015-08-12 17:52:28 +0200

    123

--- Agilent_ExtensionBox/Internal/Letch.cs
+++ Agilent_ExtensionBox/Internal/Letch.cs
@@ -1,10 +1,19 @@
-﻿using System;
+﻿using Agilent_ExtensionBox.IO;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Agilent_ExtensionBox.Internal
 {
-    class Letch
+    public class Letch
     {
+        private DigitalBit _Selectro_ADC_A0;
+        private DigitalBit _Selectro_ADC_A1;
+        private DigitalBit _LetchPulseBit;
+
+        public Letch(DigitalBit Selector_ADC_A0,DigitalBit Selector_ADC_A1, DigitalBit LetchPulseBit)
+        {
+
+        }
     }
 }
