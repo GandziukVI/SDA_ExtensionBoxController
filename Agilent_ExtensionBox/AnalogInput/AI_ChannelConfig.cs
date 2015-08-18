f41c36bf5f4eabb5d0d16ba33f5eec800700157d
GandziukVI <gandziuk2009@gmail.com>
2015-08-18 18:36:30 +0200

    Bla

---
+++ Agilent_ExtensionBox/AnalogInput/AI_ChannelConfig.cs
@@ -0,0 +1,17 @@
+﻿using Agilent.AgilentU254x.Interop;
+using Agilent_ExtensionBox.Internal;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Agilent_ExtensionBox.IO
+{
+    public class AI_ChannelConfig
+    {
+        public AnalogInChannelsEnum ChannelName { get; set; }
+        public bool Enabled { get; set; }
+        public ChannelMode Mode { get; set; }
+        public AgilentU254xAnalogPolarityEnum Polarity { get; set; }
+        public Ranges Range { get; set; }
+    }
+}
