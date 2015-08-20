adebcbbf1e3f88fcb3fe120d73b1d8b2b8384bff
zigorrom <https://github.com/zigorrom/DigitalAnalyzer.git>
2015-08-20 23:07:41 +0200

    Motor

--- Agilent_ExtensionBox/Internal/Motor.cs
+++ Agilent_ExtensionBox/Internal/Motor.cs
@@ -1,11 +1,23 @@
-﻿using System;
+﻿using Agilent_ExtensionBox.IO;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Agilent_ExtensionBox.Internal
 {
-    public class Motor
+    public class DCMotor
     {
+        private AO_Channel _channel;
+        private const double MaxVoltage = 9;
+        private const double MinVoltage = 2;
+        public DCMotor(AO_Channel Channel)
+        {
+            if (Channel == null)
+                throw new ArgumentNullException();
+            _channel = Channel;
+        }
+
+
 
     }
 }
