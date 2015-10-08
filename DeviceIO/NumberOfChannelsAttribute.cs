6d5e18557b8e023b44e4d73ba146b4973c8e405b
GandziukVI <gandziuk2009@gmail.com>
2015-10-08 18:22:18 +0200

    1

---
+++ DeviceIO/NumberOfChannelsAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeviceIO
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+    public class NumberOfChannelsAttribute
+    {
+        public int NumberOfChannels { get; private set; }
+
+        public NumberOfChannelsAttribute(int numberOfChannels)
+        {
+            NumberOfChannels = numberOfChannels;
+        }
+    }
+}
