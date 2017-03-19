8ff2b2772f3c09ab9f3dd6d1c6a22c51f0e1945a
GandziukVI <gandziuk2009@gmail.com>
2017-03-19 18:58:12 +0100

    Starting to develop FETs characterization program

---
+++ FET Characterization/Views/FET_IV.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FET_Characterization
+{
+	/// <summary>
+	/// Interaction logic for FET_IV.xaml
+	/// </summary>
+	public partial class FET_IV : UserControl
+	{
+		public FET_IV()
+		{
+			this.InitializeComponent();
+			
+			// Insert code required on object creation below this point.
+		}
+	}
+}
