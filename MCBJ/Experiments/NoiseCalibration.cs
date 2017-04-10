5e805417bd54413aa64e9b44c6cd8eed6fce85a3
GandziukVI <gandziuk2009@gmail.com>
2017-04-10 17:39:01 +0200

    Starting to implement noise calibration code

---
+++ MCBJ/Experiments/NoiseCalibration.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCBJ.Experiments
+{
+    public interface IAmplifier
+    {
+        double[] GetIntrinsicNoiseCurve();
+        double[] GetFrequencyResponceCurve();
+
+        double[,] GetCalibratedCurve();
+    }
+
+    public class AmplifierBase : IAmplifier
+    {
+        public AmplifierBase()
+        {
+
+        }
+
+        public AmplifierBase(string FormatFileName)
+        {
+
+        }
+
+        protected double[] GetIntrinsicNoiseCurve()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected double[] GetFrequencyResponceCurve()
+        {
+            throw new NotImplementedException();
+        }
+
+        public double[,] GetCalibratedCurve()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class HomamadeAmplifier
+    {
+
+    }
+
+    public class StanfordSR560Amplifier
+    {
+
+    }
+
+    public class NoiseCalibration
+    {
+
+    }
+}
