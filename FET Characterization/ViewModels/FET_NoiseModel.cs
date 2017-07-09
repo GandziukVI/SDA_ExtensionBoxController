8c6f6252c390636090e0c8fbefe187a8fab2ecd5
GandziukVI <gandziuk2009@gmail.com>
2017-07-09 22:42:42 +0200

    Starting development of noise measurement for FET

---
+++ FET Characterization/ViewModels/FET_NoiseModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Data;
+using System.ComponentModel;
+
+namespace FET_Characterization
+{
+	public class FET_NoiseModel : INotifyPropertyChanged
+	{
+		public FET_NoiseModel()
+		{
+			
+		}
+
+		#region INotifyPropertyChanged
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		private void NotifyPropertyChanged(String info)
+		{
+			if (PropertyChanged != null)
+			{
+				PropertyChanged(this, new PropertyChangedEventArgs(info));
+			}
+		}
+		#endregion
+	}
+}
