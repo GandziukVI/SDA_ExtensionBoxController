7636f73b78ee3341dd76167c8dd0ad613d6d3243
GandziukVI <gandziuk2009@gmail.com>
2018-02-13 18:03:26 +0100

    Improvement of DoubleUpDown functionality

--- FET Characterization/Views/ExtendedDoubleUpDown.xaml.cs
+++ FET Characterization/Views/ExtendedDoubleUpDown.xaml.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Globalization;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -14,19 +15,116 @@ using System.Windows.Shapes;
 
 namespace FET_Characterization
 {
+    public class SelectedIndexMultiBindingConverter : IMultiValueConverter
+    {
+        IValueConverter multConv;
+        public SelectedIndexMultiBindingConverter()
+        {
+            multConv = new MultiplierConverter();;
+        }
+
+        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return System.Convert.ToInt32(values[0]);
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture)
+        {
+            var result = new object[2];
+
+            result[0] = System.Convert.ToInt32(value);
+            result[1] = System.Convert.ToDouble(multConv.ConvertBack(value, typeof(double), null, CultureInfo.InvariantCulture), NumberFormatInfo.InvariantInfo);
+
+            return result;
+        }
+    }
+
     /// <summary>
     /// Логика взаимодействия для ExtendedDoubleUpDown.xaml
     /// </summary>
     public partial class ExtendedDoubleUpDown : UserControl
     {
+        public ExtendedDoubleUpDown()
+        {
+            this.InitializeComponent();
+
+            
+            var multiValueConverter = new SelectedIndexMultiBindingConverter();
+
+            var multiBinding = new MultiBinding() { NotifyOnSourceUpdated = true };
+
+            multiBinding.Converter = multiValueConverter;
+
+            multiBinding.Bindings.Add(new Binding("MultiplierIndex") { Source = this, Mode = BindingMode.TwoWay, UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged });
+            multiBinding.Bindings.Add(new Binding("Multiplier") { Source = this, Mode = BindingMode.TwoWay, UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged });
+
+            DataMultiplier.SetBinding(ComboBox.SelectedIndexProperty, multiBinding);
+
+            var formatStrBinding = new Binding("FormatString") { Source = this, Mode = BindingMode.TwoWay, UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged, NotifyOnSourceUpdated = true };
+
+            DataInput.SetBinding(Xceed.Wpf.Toolkit.DoubleUpDown.FormatStringProperty, formatStrBinding);
+        }
+
+        // Flags for properties
         static FrameworkPropertyMetadataOptions flags = FrameworkPropertyMetadataOptions.BindsTwoWayByDefault | FrameworkPropertyMetadataOptions.AffectsMeasure;
 
-        static FrameworkPropertyMetadata ValuePropertyMetadata = new FrameworkPropertyMetadata(Double.NaN, flags, new PropertyChangedCallback(onValuePropertyChanged), new CoerceValueCallback(CoerseValue));
+        #region Value property
+
+        public double Value
+        {
+            get { return (double)GetValue(ValueProperty); }
+            set { SetValue(ValueProperty, value); }
+        }
+
+        static FrameworkPropertyMetadata ValuePropertyMetadata = new FrameworkPropertyMetadata(Double.NaN, flags, new PropertyChangedCallback(onValuePropertyChanged));
         static readonly DependencyProperty ValueProperty = DependencyProperty.Register(
             "Value",
             typeof(Double),
             typeof(ExtendedDoubleUpDown),
             ValuePropertyMetadata);
+
+        static void onValuePropertyChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            var control = sender as ExtendedDoubleUpDown;
+            control.Value = System.Convert.ToDouble(e.NewValue, NumberFormatInfo.InvariantInfo);
+
+            var newRealValue = new DependencyPropertyChangedEventArgs(
+                RealValueProperty, sender.GetValue(RealValueProperty),
+                System.Convert.ToDouble(e.NewValue) * System.Convert.ToDouble(sender.GetValue(MultiplierProperty)));
+
+            onRealValuePropertyChanged(sender, newRealValue);
+        }
+
+        #endregion
+
+        #region RealValue property
+
+        public double RealValue
+        {
+            get { return (Double)GetValue(RealValueProperty); }
+            set { SetValue(RealValueProperty, value); }
+        }
+
+        static FrameworkPropertyMetadata RealValueMetadata = new FrameworkPropertyMetadata(double.NaN, flags, new PropertyChangedCallback(onRealValuePropertyChanged));
+        public static readonly DependencyProperty RealValueProperty = DependencyProperty.Register(
+            "RealValue",
+            typeof(Double),
+            typeof(ExtendedDoubleUpDown),
+            RealValueMetadata);
+
+        static void onRealValuePropertyChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+        }
+
+        #endregion
+
+        #region FormatString property
+
+        public string FormatString
+        {
+            get { return (string)GetValue(FormatStringProperty); }
+            set { SetValue(FormatStringProperty, value); }
+        }
 
         static FrameworkPropertyMetadata FormatStringMetadata = new FrameworkPropertyMetadata("F3", flags, new PropertyChangedCallback(onFormatStringPropertyChanged));
         public static readonly DependencyProperty FormatStringProperty = DependencyProperty.Register(
@@ -35,51 +133,26 @@ namespace FET_Characterization
             typeof(ExtendedDoubleUpDown),
             FormatStringMetadata);
 
+        static void onFormatStringPropertyChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+        }
+
+        #endregion
+
+        #region UnitAlias property
+
+        public String UnitAlias
+        {
+            get { return (String)GetValue(UnitAliasProperty); }
+            set { SetValue(UnitAliasProperty, value); }
+        }
+
         static FrameworkPropertyMetadata UnitAliasMetadata = new FrameworkPropertyMetadata("", flags, new PropertyChangedCallback(onUnitAliasPropertyChanged), new CoerceValueCallback(CoerseUnitAlias));
         public static readonly DependencyProperty UnitAliasProperty = DependencyProperty.Register(
             "UnitAlias",
             typeof(String),
             typeof(ExtendedDoubleUpDown),
             UnitAliasMetadata);
-
-        static FrameworkPropertyMetadata MultiplierMetadata = new FrameworkPropertyMetadata(1.0, flags, new PropertyChangedCallback(onMultiplierPropertyChanged));
-        public static readonly DependencyProperty MultiplierProperty = DependencyProperty.Register(
-            "Multiplier",
-            typeof(Double),
-            typeof(ExtendedDoubleUpDown),
-            MultiplierMetadata);
-
-        //static FrameworkPropertyMetadata MultiplierIndexMetadata = new FrameworkPropertyMetadata(1, flags, new PropertyChangedCallback(onMultiplierIndexPropertyChanged), new CoerceValueCallback(CoerceMultiplierIndex));
-        //public static readonly DependencyProperty MultiplierIndexProperty = DependencyProperty.Register(
-        //    "MultiplierIndex",
-        //    typeof(Int32),
-        //    typeof(ExtendedDoubleUpDown),
-        //    MultiplierIndexMetadata);
-
-        static FrameworkPropertyMetadata RealValueMetadata = new FrameworkPropertyMetadata(double.NaN, flags, new PropertyChangedCallback(onRealValuePropertyChanged), new CoerceValueCallback(CoerceRealValue));
-        public static readonly DependencyProperty RealValueProperty = DependencyProperty.Register(
-            "RealValue",
-            typeof(Double),
-            typeof(ExtendedDoubleUpDown),
-            RealValueMetadata);
-
-        static void onValuePropertyChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
-        {
-            var control = sender as ExtendedDoubleUpDown;
-            control.Value = (Double)e.NewValue;
-            onRealValuePropertyChanged(sender, e);
-        }
-
-        static object CoerseValue(DependencyObject sender, object value)
-        {
-            return value;
-        }
-
-        static void onFormatStringPropertyChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
-        {
-            var control = sender as ExtendedDoubleUpDown;
-            control.FormatString = (string)e.NewValue;
-        }
 
         static void onUnitAliasPropertyChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
         {
@@ -117,59 +190,9 @@ namespace FET_Characterization
             return result;
         }
 
-        static void onMultiplierPropertyChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
-        {
-            var control = sender as ExtendedDoubleUpDown;
-            control.Multiplier = (double)e.NewValue;
-            onRealValuePropertyChanged(sender, e);
-        }
-        
-        //static void onMultiplierIndexPropertyChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
-        //{
-        //    var control = sender as ExtendedDoubleUpDown;
-        //    control.MultiplierIndex = (Int32)e.NewValue;
-        //    control.DataMultiplier.SelectedIndex = (Int32)e.NewValue;
-        //    onMultiplierPropertyChanged(sender, e);
-        //}
+        #endregion
 
-        static object CoerceMultiplierIndex(DependencyObject sender, object value)
-        {
-            return value;
-        }
-
-        static void onRealValuePropertyChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
-        {
-            var control = sender as ExtendedDoubleUpDown;
-            control.RealValue = control.Value * control.Multiplier;
-        }
-
-        static object CoerceRealValue(DependencyObject sender, object value)
-        {
-            return value;
-        }
-
-        public ExtendedDoubleUpDown()
-        {
-            this.InitializeComponent();
-        }
-
-        public double Value
-        {
-            get { return (double)GetValue(ValueProperty); }
-            set { SetValue(ValueProperty, value); }
-        }
-
-        public string FormatString
-        {
-            get { return (string)GetValue(FormatStringProperty); }
-            set { SetValue(FormatStringProperty, value); }
-        }
-
-        public String UnitAlias
-        {
-            get { return (String)GetValue(UnitAliasProperty); }
-            set { SetValue(UnitAliasProperty, value); }
-        }
+        #region Multiplier property
 
         public double Multiplier
         {
@@ -177,16 +200,45 @@ namespace FET_Characterization
             set { SetValue(MultiplierProperty, value); }
         }
 
-        //public Int32 MultiplierIndex
-        //{
-        //    get { return Convert.ToInt32(GetValue(MultiplierIndexProperty)); }
-        //    set { SetValue(MultiplierIndexProperty, value); }
-        //}
+        static FrameworkPropertyMetadata MultiplierMetadata = new FrameworkPropertyMetadata(1.0, flags, new PropertyChangedCallback(onMultiplierPropertyChanged));
 
-        public double RealValue
+        static void onMultiplierPropertyChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
         {
-            get { return (Double)GetValue(RealValueProperty); }
-            set { SetValue(RealValueProperty, value); }
+            var newRealValue = new DependencyPropertyChangedEventArgs(
+                RealValueProperty,
+                sender.GetValue(RealValueProperty),
+                System.Convert.ToDouble(sender.GetValue(ValueProperty), NumberFormatInfo.InvariantInfo) * System.Convert.ToDouble(e.NewValue, NumberFormatInfo.InvariantInfo));
+
+            onRealValuePropertyChanged(sender, newRealValue);
         }
+
+        public static readonly DependencyProperty MultiplierProperty = DependencyProperty.Register(
+            "Multiplier",
+            typeof(Double),
+            typeof(ExtendedDoubleUpDown),
+            MultiplierMetadata);
+
+        #endregion
+
+        #region MultiplierIndex property
+
+        public int MultiplierIndex
+        {
+            get { return (int)GetValue(MultiplierIndexProperty); }
+            set { SetValue(MultiplierIndexProperty, value); }
+        }
+
+        static FrameworkPropertyMetadata MultiplierIndexMetadata = new FrameworkPropertyMetadata(0, flags, new PropertyChangedCallback(onMultiplierIndexPropertyChanged));
+
+        private static void onMultiplierIndexPropertyChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+        }
+        
+        public static readonly DependencyProperty MultiplierIndexProperty =
+            DependencyProperty.Register("MultiplierIndex", typeof(int), typeof(ExtendedDoubleUpDown), MultiplierIndexMetadata);
+
+        
+
+        #endregion
     }
 }
