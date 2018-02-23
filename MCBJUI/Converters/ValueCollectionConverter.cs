0842556d20d4e5c26f37d6cb136493dfe0ac0910
GandziukVI <gandziuk2009@gmail.com>
2018-02-23 20:56:59 +0100

    Refactoring

---
+++ MCBJUI/Converters/ValueCollectionConverter.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace MCBJUI.Converters
+{
+    [ValueConversion(typeof(string), typeof(double[]))]
+    public class ValueCollectionConverter : IValueConverter
+    {
+        char[] separators = "[], ".ToCharArray();
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            var values = (double[])value;
+
+            var sb = new StringBuilder();
+            sb.Append("[ ");
+
+            for (int i = 0; i < values.Length; i++)
+                sb.AppendFormat("{0}, ", values[i].ToString("0.000", NumberFormatInfo.InvariantInfo));
+
+            sb.Append("]");
+
+            return sb.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            var arrayString = (string)value;
+
+            var query = from item in arrayString.Split(separators, StringSplitOptions.RemoveEmptyEntries)
+                        select double.Parse(item, NumberFormatInfo.InvariantInfo);
+
+            return query.ToArray();
+        }
+    }
+}
---
+++ MCBJUI/Views/NoiseDefRSettingsControl.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MCBJUI
+{
+	/// <summary>
+	/// Interaction logic for NoiseDefRSettingsControl.xaml
+	/// </summary>
+	public partial class NoiseDefRSettingsControl : UserControl
+	{
+		System.Windows.Forms.FolderBrowserDialog dialog;
+		
+		public NoiseDefRSettingsControl()
+		{
+			this.InitializeComponent();
+			
+			// Insert code required on object creation below this point.
+		}
+
+		private void SelectAddress(object sender, System.Windows.RoutedEventArgs e)
+        {
+            TextBox tb = (sender as TextBox);
+
+            if (tb != null)
+            {
+                tb.SelectAll();
+            }
+        }
+
+        private void SelectivelyIgnoreMouseButton(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            TextBox tb = (sender as TextBox);
+
+            if (tb != null)
+            {
+                if (!tb.IsKeyboardFocusWithin)
+                {
+                    e.Handled = true;
+                    tb.Focus();
+                }
+            }
+        }  
+
+        private void on_cmdOpenFolderClick(object sender, System.Windows.RoutedEventArgs e)
+        {
+            dialog.ShowDialog();
+            (DataContext as NoiseDefRSettingsControlModel).FilePath = dialog.SelectedPath;
+        }
+		
+		private void on_MCBJ_OpenDataFolder_Click(object sender, System.Windows.RoutedEventArgs e)
+        {        	
+            var startInfo = new ProcessStartInfo() { UseShellExecute = true, Verb = "open" };
+
+            dialog.SelectedPath = (DataContext as NoiseDefRSettingsControlModel).FilePath;
+            startInfo.FileName = dialog.SelectedPath;
+
+            Process.Start(startInfo);
+        }        
+	}
+}
