d3e223006d6dd7f2c69929879b7ca7e84ddcb41c
Volodymyr Handziuk <gandziuk2009@gmail.com>
2019-01-11 11:54:19 +0100

    Starting implementation of the experiment log

---
+++ MCBJ/Experiments/DataHandling/ExperimentLog.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCBJ.Experiments.DataHandling
+{
+    public class ExperimentLog
+    {
+        public string FileName { get; set; }
+        
+    }
+}
