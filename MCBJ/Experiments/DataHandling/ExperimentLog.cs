07b74c582405b235c0f695b935a3aba335ce64b2
Volodymyr Handziuk <gandziuk2009@gmail.com>
2019-01-11 12:49:30 +0100

    Adding simple implementation to experiment logging

--- MCBJ/Experiments/DataHandling/ExperimentLog.cs
+++ MCBJ/Experiments/DataHandling/ExperimentLog.cs
@@ -1,14 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MCBJ.Experiments.DataHandling
-{
+{    
     public class ExperimentLog
     {
         public string FileName { get; set; }
-        
+        public string FilePath { get; set; }
+
+        public void WriteToLog(string messageType, string messageString)
+        {
+            if(string.IsNullOrEmpty(FilePath))
+                FilePath = Directory.GetCurrentDirectory();
+            if(string.IsNullOrEmpty(FileName))
+                FileName = "ExperimentLog.txt";
+
+            var path = FilePath + "\\" + FileName;
+            if(!File.Exists(path))
+                File.Create(path);
+
+            using(var fs = new FileStream(path, FileMode.Append, FileAccess.Write))
+                using(var sw = new StreamWriter(fs))
+                    sw.WriteLine(string.Format("%s\t%s", messageType, messageString));                            
+        }
     }
 }
